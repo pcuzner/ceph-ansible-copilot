8f48b97bddcb912a6a8e30e4d6da85213e951e41
Paul Cuzner <pcuzner@redhat.com>
2017-12-11 15:20:58 +1300

    copilot: init only runs the ansible.cfg updated when called from copilot
    This just prevents an import from modifying your ansible.cfg file

--- ceph_ansible_copilot/__init__.py
+++ ceph_ansible_copilot/__init__.py
@@ -1,9 +1,12 @@
 
+import sys
+
 from .hosts import Host
 from .utils import setup_ansible_cfg
 
 __version__ = '0.9.1'
 
-setup_ansible_cfg()
+if sys.argv[0] == '/usr/bin/copilot':
+    setup_ansible_cfg()
 
 
