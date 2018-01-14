9285263fdcb2ca7c1b2be5771a86b6a1bf7df7b1
Paul Cuzner <pcuzner@redhat.com>
2018-01-15 09:43:01 +1300

    color palette move to it's own module, so it can be imported anywhere

---
+++ ceph_ansible_copilot/ui/palette.py
@@ -0,0 +1,24 @@
+
+# Color palette for the UI
+
+palette = [
+    ('body', 'black', 'light gray'),
+    ('message', 'light gray', 'black'),
+    ('reversed', 'light gray', 'black'),
+    ('reversed_green', 'light gray', 'dark green'),
+    ('reversed_red', 'light gray', 'dark red'),
+    ('enabled_input', 'white', 'dark blue'),
+    ('bkgnd_white', 'black', 'white'),
+    ('error_message', 'white', 'dark red'),
+    ('warning_message', 'black', 'brown'),
+    ('error_box', 'dark red', 'light gray'),
+    ('key', 'light cyan', 'black', 'underline'),
+    ('title', 'white', 'dark blue'),
+    ('active_step', 'black', 'light gray'),
+    ('inactive_step', 'white', 'dark red'),
+    ('editbox', 'black', 'light gray'),
+    ('buttn', 'black', 'dark cyan'),
+    ('buttnf', 'white', 'dark blue', 'bold'),
+    ('pg_normal', 'white', 'dark blue'),
+    ('pg_complete', 'white', 'white')
+]
