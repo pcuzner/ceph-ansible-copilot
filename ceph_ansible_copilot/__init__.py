3e67495f11c1fd5e10a578b40c1f9dc3b8435ef8
Paul Cuzner <pcuzner@redhat.com>
2018-04-13 16:14:26 +1200

    interim version bump for testing

--- ceph_ansible_copilot/__init__.py
+++ ceph_ansible_copilot/__init__.py
@@ -4,9 +4,7 @@ import sys
 from .hosts import Host
 from .utils import setup_ansible_cfg
 
-__version__ = '0.9.5'
+__version__ = '0.9.6a9'
 
 if sys.argv[0] == '/usr/bin/copilot':
     setup_ansible_cfg()
-
-
